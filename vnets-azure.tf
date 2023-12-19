cd1bca229f5554252e075301600237429d334487
Jason Adams <jasonadams78@hotmail.com>
2023-12-19 08:40:59 -0600

    Screwd up some subnets.

--- vnets-azure.tf
+++ vnets-azure.tf
@@ -22,22 +22,22 @@ variable "GatewaySnetAddrespace" {
 
 variable "mgmtSnetAddrspace" {
     type = tuple([ string ])
-    default = ["10.0.2.0/16"]
+    default = ["10.0.2.0/24"]
 }
 
 variable "DEVAddrspace" {
     type = tuple([ string ])
-    default = ["10.0.3.0/16"]
+    default = ["10.0.3.0/24"]
 }
 
 variable "UATAddrspace" {
     type = tuple([ string ])
-    default = ["10.0.4.0/16"]
+    default = ["10.0.4.0/24"]
 }
 
 variable "PRODAddrspace" {
     type = tuple([ string ])
-    default = ["10.0.5.0/16"]
+    default = ["10.0.5.0/24"]
 }
 
 
